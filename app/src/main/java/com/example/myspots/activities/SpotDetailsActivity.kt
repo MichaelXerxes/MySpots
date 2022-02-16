07350b068aee41fa9842fa351b38f97ed855f1a2
MichaelXerxes <81194285+MichaelXerxes@users.noreply.github.com>
2022-02-16 15:32:20 +0000

    swipe and onClick cdn...

--- app/src/main/java/com/example/myspots/activities/SpotDetailsActivity.kt
+++ app/src/main/java/com/example/myspots/activities/SpotDetailsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myspots.activities
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +10,7 @@ import com.example.myspots.models.SpotModel
 
 class SpotDetailsActivity : AppCompatActivity() {
     private var binding:ActivitySpotDetailsBinding?=null
+    private val uri="geo:0,0?q=india"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivitySpotDetailsBinding.inflate(layoutInflater)
@@ -29,6 +31,20 @@ class SpotDetailsActivity : AppCompatActivity() {
 
             supportActionBar!!.title=spotModel.title
             binding!!.imageViewDetailsID.setImageURI(Uri.parse(spotModel.image))
+            binding!!.tvDescriptionD.text=spotModel.description
+            binding!!.tvLocationD.text=spotModel.location
+            binding!!.tvDataD.text=spotModel.date
+            binding!!.tvLatitD.text=spotModel.latitude.toString()
+            binding!!.tvLongiD.text=spotModel.longitude.toString()
+        }
+
+        binding!!.btnViewOnMap.setOnClickListener {
+            val uriMap=Uri.parse(uri)
+            val intent=Intent(Intent.ACTION_VIEW,uriMap)
+            intent.setPackage("com.google.android.apps.maps")
+            if(intent.resolveActivity(packageManager)!=null){
+                startActivity(intent)
+            }
         }
 
 
